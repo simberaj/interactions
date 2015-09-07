8ece1ed7ec7fe3f64a8a2faf4bfe2282d7bac9cb
simberaj <simbera.jan@gmail.com>
2015-09-07 18:26:17 +0200

    Revert "Initial commit"
    This reverts commit ef7e175dc9c6808b1d112b81b38133cd2f465b72.

--- delimit_regions.py
+++
@@ -1,6 +0,0 @@
-import common, regionalization
-
-with common.runtool(regionalization.getMainParamCount()) as parameters:
-  # import cProfile
-  # cProfile.run('regionalization.runByParams(*parameters, delimit=True)')
-  regionalization.runByParams(*parameters, delimit=True)
